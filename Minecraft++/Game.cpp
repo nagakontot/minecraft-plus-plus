549000bc3c05da8fdca48abc542f6994703cece1
retep998 <retep998@75d76499-6439-955b-e7ff-335baec30d82>
2010-12-15 02:31:09 +0000

    committing like a boss

--- Minecraft++/Game.cpp
+++ Minecraft++/Game.cpp
@@ -5,10 +5,12 @@
 bool Game::Init() {
 	InitBlocks();
 	Window.Create(sf::VideoMode(800,600,32),"Minecraft++",sf::Style::Close|sf::Style::Titlebar,sf::ContextSettings(24,0,0));
+	InitGraphics();
 	srand(clock());
 	return true;
 }
 bool Game::Loop() {
+	//Event handling
 	sf::Event e;
 	while(Window.GetEvent(e)){
 		switch(e.Type){
@@ -16,10 +18,12 @@ bool Game::Loop() {
 			return false;
 		}
 	}
-	uint64_t n1, n2;
-	cin >> n1 >> n2;
-	cout << random(n1, n2) << endl;
-	return true;
+	//Clear the screen
+	glClear(GL_COLOR_BUFFER_BIT);
+	//Draw everything
+	//Display the screen
+	Window.Display();
+	return Window.IsOpened();
 }
 void Game::Unload() {
 
--- Minecraft++/Graphics.cpp
+++ Minecraft++/Graphics.cpp
@@ -1,3 +1,13 @@
 #include "Global.h"
 
 sf::Window Window;
+
+void InitGraphics() {
+	glClearColor(0,0,0,0);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_TEXTURE_2D);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glMatrixMode(GL_PROJECTION);
+	gluPerspective(90,4/3,0,1000);
+}
