284bfd9535f40992ecaa43940abc93d64aef868c
retep998 <retep998@75d76499-6439-955b-e7ff-335baec30d82>
2010-12-15 03:27:36 +0000

    rendering a green square is working.

--- Minecraft++/Graphics.cpp
+++ Minecraft++/Graphics.cpp
@@ -5,9 +5,13 @@ sf::Window Window;
 void InitGraphics() {
 	glClearColor(0,0,0,0);
 	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
 	glEnable(GL_TEXTURE_2D);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
 	gluPerspective(90,4/3,0,1000);
+	gluLookAt(0,0,-100,100,100,0,0,0,-1);
+
 }
