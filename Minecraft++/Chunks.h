6165e77600689e85dd0955818af81676e63f957d
retep998 <retep998@75d76499-6439-955b-e7ff-335baec30d82>
2010-12-19 22:23:12 +0000

    Multi threading for the win. Boost is now a dependency

--- Minecraft++/Chunks.h
+++ Minecraft++/Chunks.h
@@ -4,12 +4,21 @@ struct Chunk {
 	Block Blocks[4096];
 	GLfloat *model, *tex;
 	uint16_t verts;
+	boost::mutex lock;
+	bool generated;
+	bool updated;
 	Chunk(int64_t x, int64_t y, int64_t z);
 	const void Draw();
 	void Update();
+	void Generate();
 };
 
 Chunk* GetChunk(int64_t x, int64_t y, int64_t z, bool generate = true);
 
 extern vector<Chunk*> Chunks;
 extern map<int64_t,map<int64_t,map<int64_t,Chunk*>>> ChunkPos;
+
+void ChunkUpdateThread();
+void AddChunkUpdate(Chunk* c);
+extern queue<Chunk*> ChunksToUpdate;
+extern boost::mutex ChunkUpdate;
